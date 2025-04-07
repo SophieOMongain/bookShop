55099d7518e72d751bb715d4d585fd96c5e1935c
SophieOMongain <146173098+SophieOMongain@users.noreply.github.com>
2025-04-07 20:52:08 +0100

    Added repository classes for Book, User, Cart, and Item

---
+++ src/main/java/entity/Cart.java
@@ -0,0 +1,55 @@
+package entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
+import java.util.List;
+
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<Item> items;
+    
+    public Cart() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public User getUser() {
+        return user;
+    }
+    
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    public List<Item> getItems() {
+        return items;
+    }
+    
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
+
