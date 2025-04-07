b9969195c8ee4001186f267283adc506d8ac1ecd
SophieOMongain <146173098+SophieOMongain@users.noreply.github.com>
2025-04-07 20:54:59 +0100

    Add new repository and entity classes

--- src/main/java/repository/CartRepository.java
+++ src/main/java/repository/CartRepository.java
@@ -1,5 +1,9 @@
 package repository;
 
-public class CartRepository {
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.Cart;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
--- src/main/java/repository/ItemRepository.java
+++ src/main/java/repository/ItemRepository.java
@@ -1,5 +1,9 @@
 package repository;
 
-public class ItemRepository {
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entity.Item;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
